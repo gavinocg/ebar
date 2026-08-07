bf7d4c076929c3815c6966b440d3ce315770096d
Gavino Carranco <gcarranco@hotmail.com>
2026-08-06 23:16:27 -0500

    Corregir import View en ControladorCaja

--- app/Http/Controllers/ControladorCaja.php
+++ app/Http/Controllers/ControladorCaja.php
@@ -14,6 +14,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class ControladorCaja extends Controller
 {
