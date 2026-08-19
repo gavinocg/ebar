64c41adb723e1482922a7f4daac29596020881bd
Gavino Carranco <gcarranco@hotmail.com>
2026-08-19 00:13:58 -0500

    chore: remove temp check_braces.php

--- check_braces.php
+++
@@ -1,9 +0,0 @@
-<?php
-$code = file_get_contents('app/Http/Controllers/ControladorReportes.php');
-$open = 0;
-$close = 0;
-for ($i = 0; $i < strlen($code); $i++) {
-    if ($code[$i] === '{') $open++;
-    if ($code[$i] === '}') $close++;
-}
-echo "Open: $open, Close: $close, Balance: " . ($open - $close) . "\n";
